f2489c08249701ae9bbf5747b3eee0282ab4b6ef
revze <revze@outlook.co.id>
2017-08-13 14:47:04 +0700

    Fixed bug in server module

--- server/DATABASE.sql
+++ server/DATABASE.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 12 Agu 2017 pada 18.15
--- Versi Server: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: 13 Agu 2017 pada 09.44
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -38,7 +40,7 @@ CREATE TABLE `api_tokens` (
 --
 
 INSERT INTO `api_tokens` (`id`, `token`, `created_at`, `updated_at`) VALUES
-(1, 'ijdasoijds09d098', '2017-08-11 20:43:22', '2017-08-11 20:43:22');
+(1, 'ijdasoijds09d098', '2017-08-13 00:20:16', '2017-08-13 00:20:16');
 
 -- --------------------------------------------------------
 
@@ -59,22 +61,8 @@ CREATE TABLE `endless_runnings` (
 --
 
 INSERT INTO `endless_runnings` (`id`, `score`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 30, 2, '2017-08-11 21:31:44', '2017-08-11 21:31:44'),
-(2, 40, 2, '2017-08-11 21:31:52', '2017-08-11 21:31:52'),
-(3, 50, 2, '2017-08-11 21:31:57', '2017-08-11 21:31:57'),
-(4, 60, 1, '2017-08-11 21:32:17', '2017-08-11 21:32:17'),
-(5, 60, 1, '2017-08-11 21:35:58', '2017-08-11 21:35:58'),
-(6, 90, 4, '2017-08-12 07:11:37', '2017-08-12 07:11:37'),
-(7, 180, 4, '2017-08-12 07:16:48', '2017-08-12 07:16:48'),
-(8, 90, 4, '2017-08-12 07:17:02', '2017-08-12 07:17:02'),
-(9, 180, 5, '2017-08-12 07:25:25', '2017-08-12 07:25:25'),
-(10, 180, 5, '2017-08-12 07:25:55', '2017-08-12 07:25:55'),
-(11, 90, 5, '2017-08-12 07:26:17', '2017-08-12 07:26:17'),
-(12, 170, 5, '2017-08-12 07:27:40', '2017-08-12 07:27:40'),
-(13, 180, 1, '2017-08-12 07:45:43', '2017-08-12 07:45:43'),
-(14, 90, 1, '2017-08-12 07:47:45', '2017-08-12 07:47:45'),
-(15, 250, 1, '2017-08-12 09:12:26', '2017-08-12 09:12:26'),
-(16, 90, 7, '2017-08-12 09:14:25', '2017-08-12 09:14:25');
+(1, 260, 1, '2017-08-13 00:30:45', '2017-08-13 00:30:45'),
+(2, 260, 2, '2017-08-13 00:31:58', '2017-08-13 00:31:58');
 
 -- --------------------------------------------------------
 
@@ -93,22 +81,10 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(9, '2014_10_12_000000_create_users_table', 1),
-(10, '2017_08_07_152539_create_endless_runnings_table', 1),
-(11, '2017_08_07_152813_create_tetris_table', 1),
-(12, '2017_08_07_163517_create_api_tokens_table', 1);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2017_08_07_152539_create_endless_runnings_table', 1),
+(3, '2017_08_07_152813_create_tetris_table', 1),
+(4, '2017_08_07_163517_create_api_tokens_table', 1);
 
 -- --------------------------------------------------------
 
@@ -129,11 +105,10 @@ CREATE TABLE `tetris` (
 --
 
 INSERT INTO `tetris` (`id`, `score`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 60, 1, '2017-08-11 21:39:17', '2017-08-11 21:39:17'),
-(2, 70, 1, '2017-08-11 21:39:34', '2017-08-11 21:39:34'),
-(3, 90, 1, '2017-08-11 21:39:41', '2017-08-11 21:39:41'),
-(4, 100, 1, '2017-08-11 21:39:46', '2017-08-11 21:39:46'),
-(5, 100, 1, '2017-08-11 21:43:29', '2017-08-11 21:43:29');
+(1, 50, 1, '2017-08-13 00:43:47', '2017-08-13 00:43:47'),
+(2, 70, 1, '2017-08-13 00:43:57', '2017-08-13 00:43:57'),
+(3, 80, 2, '2017-08-13 00:44:09', '2017-08-13 00:44:09'),
+(4, 100, 2, '2017-08-13 00:44:18', '2017-08-13 00:44:18');
 
 -- --------------------------------------------------------
 
@@ -160,13 +135,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `date_birth`, `phone_number`, `profile_picture`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Revando', 'revze', 'revando@outlook.com', '$2y$10$DwXWTowSWgNQMCTOKpmWM.Wx06LQIcpZl36gomn7Z2VdTpSvQ2xX6', '1999-05-19', '085816865807', 'revze.png', 'jaQcXgqbqhOI9SPCRkS6j4kQUQzYsaP9Hu77Ew8pX30wm1CFnsAItG6C9Yxf', '2017-08-11 20:43:22', '2017-08-11 20:43:22'),
-(2, 'Revando 2', 'revando', 'dev.revze@gmail.com', '$2y$10$VeF6/8iBKhDhw5RuM.VgSOb00zH4Fmnc9ebHLuoFTGvPoQmo/1cvy', '1999-09-19', '0999999', '1502509911.png', 'l4mLv4DArccyKQz81gHNkadv6SdSxPKErGUcrljF7YZi3Qm6ZSiucT4Iqk0K', '2017-08-11 20:46:28', '2017-08-11 20:51:51'),
-(3, 'asdasd', 'sadasd', 'dedyqurnia722@gmail.comasd', '$2y$10$ZOtFZ2aWEJCjNckymSQFqO.1kHXGBWUehQCfbPV/Xoqn2n0UuXH6S', '2017-08-16', '213213', '1502544920.png', NULL, '2017-08-12 06:35:20', '2017-08-12 06:35:20'),
-(4, 'Abri Asyikin', 'abri', 'abri@gmail.com', '$2y$10$2pU7PON03RZxxJlJ2NWuneg.jErOiDm3qcZBkQeeSvbtwaU7B6xr2', '2017-08-23', '9999', '1502545399.jpg', 'gtBGGfndyoyW5Z0T0Nesy2NWvRDjF5WCciDpufDRlDJQrIi8ugJ66zHxqZzr', '2017-08-12 06:43:18', '2017-08-12 06:43:18'),
-(5, 'Erwin', 'erwin', 'erwin@gmail.com', '$2y$10$p/F.Co5ir.1jW397iAcM/ehaWh7GpdGOEcq5yX790a4VZEB4VnS.W', '2017-08-23', '999999', '1502547898.jpg', NULL, '2017-08-12 07:24:58', '2017-08-12 07:24:58'),
-(6, 'Ismail', 'ismail', 'ismail@gmail.com', '$2y$10$X542FqwbY9uAAl8EdUZAoeviMNlx7jE6x.aKjNgrtTKDD/fn5exNK', '2017-08-15', '45454545', '1502549848.jpg', NULL, '2017-08-12 07:57:28', '2017-08-12 07:57:28'),
-(7, 'Edwin', 'edwin', 'edwin@gmail.com', '$2y$10$M.WA7vAj4hH.dOwpymOE/.QDVK7.ktHKBiaR75qEUZiITpE1Lqpii', '2017-08-24', '45435345', '1502554445.png', NULL, '2017-08-12 09:14:05', '2017-08-12 09:14:05');
+(1, 'Revando', 'revze', 'revando@outlook.com', '$2y$10$1f.gJdjZq3wjPVXS7T6/KOI19iQgcEKnnueTDliQzCGvep/mChYLG', '1999-05-19', '085816865807', 'revze.png', 'Dt3smO1c6cKeeniU40t415GnRVIuAi5ePjYQvDQQDB2iNfDPeymcnMbI0K56', '2017-08-13 00:20:16', '2017-08-13 00:20:16'),
+(2, 'Friezky', 'friezky', 'friezky@gmail.com', '$2y$10$nxNDXhBxs6ftF8FsARe9SePggJHHDTXTj4oWevwu7J2xmG1olRCaC', '2017-08-24', '12121213', '1502609479.png', 'zHgTSQIqfcOkdge4OJN4S1qH735Xm8OU9WV37fh7KVXvdZkqXWdFalIIvIHS', '2017-08-13 00:31:19', '2017-08-13 00:31:19');
 
 --
 -- Indexes for dumped tables
@@ -217,22 +187,22 @@ ALTER TABLE `api_tokens`
 -- AUTO_INCREMENT for table `endless_runnings`
 --
 ALTER TABLE `endless_runnings`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tetris`
 --
 ALTER TABLE `tetris`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
@@ -248,6 +218,7 @@ ALTER TABLE `endless_runnings`
 --
 ALTER TABLE `tetris`
   ADD CONSTRAINT `tetris_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
